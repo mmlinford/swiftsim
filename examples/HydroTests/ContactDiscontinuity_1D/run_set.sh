f30aaca4483d97b75944ec71605029bce69e0127
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-02-27 11:41:11 +0000

    Added short script to run a set of simulations with different Beta diffusion values

---
+++ examples/HydroTests/ContactDiscontinuity_1D/run_set.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+beta=(1.0 0.1 0.01 0.001)
+swift_location="../../swift"
+flags="--hydro --limiter --threads=2"
+parameter="contact.yml"
+make_ic_script="makeIC.py"
+plot_script="plotSolution.py"
+
+for i in ${beta[@]};
+do
+    mkdir beta_$i
+    cd beta_$i
+
+    python ../$make_ic_script
+
+    ../$swift_location $flags -P "SPH:diffusion_beta:${i}" ../$parameter
+
+    python ../$plot_script
+
+    rm *.hdf5
+
+    cd ..
+done
