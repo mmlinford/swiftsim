668e2eaeba5ccad4e084f40ec680131491c33f88
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-02-27 12:26:13 +0000

    Minor fix to the run script

--- examples/HydroTests/Diffusion_1D/run.sh
+++ examples/HydroTests/Diffusion_1D/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
  # Generate the initial conditions if they are not present.
-if [ ! -e contact.hdf5 ]
+if [ ! -e diffusion.hdf5 ]
 then
     echo "Generating initial conditions for the Sedov blast example..."
     python makeIC.py
 fi
 
 # Run SWIFT
-../../swift --hydro --limiter --threads=1 contact.yml 2>&1 | tee output.log
+../../swift --hydro --limiter --threads=1 diffusion.yml 2>&1 | tee output.log
