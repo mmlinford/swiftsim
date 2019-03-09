740adf5888821b15c949e3fabe41487bd1283f70
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2019-03-09 14:53:19 +0100

    Added functions to compute the feedback properties of stars in the EAGLE model. Improved documentation of most star-related functions in the EAGLE model.

---
+++ src/stars/EAGLE/evolution.c
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * This file is part of SWIFT.
+ * Copyright (c) 2019 Matthieu Schaller (schaller@strw.leidenuniv.nl)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+/**
+ * @file src/stars/EAGLE/evolution.c
+ * @brief EAGLE functions related to the evolution of star properties.
+ */
+
+/* Config parameters. */
+#include "../config.h"
+
+/* Some standard headers. */
+#include <math.h>
+
+
+/* Local includes */
+#include "error.h"
+#include "cosmology.h"
+#include "stars_part.h"
+#include "stars.h"
+
+void stars_evolve_spart(struct spart* restrict sp,
+			const struct stars_props* stars_properties,
+			const struct cosmology* cosmo) {
+
+}
