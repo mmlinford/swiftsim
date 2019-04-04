400fbc80b6b9655a0015fc3620882c365b422df7
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2019-04-04 17:11:49 +0100

    Do not restrict the cooling rate in the EAGLE cooling by using 2.5x the time-step size.

--- src/cooling/EAGLE/cooling.c
+++ src/cooling/EAGLE/cooling.c
@@ -57,7 +57,7 @@ static const int bisection_max_iterations = 150;
 static const float explicit_tolerance = 0.05;
 static const float newton_tolerance = 1.0e-4;
 static const float bisection_tolerance = 1.0e-6;
-static const float rounding_tolerance = 1.0e-4;
+// static const float rounding_tolerance = 1.0e-4;
 static const double bracket_factor = 1.5;
 static const double newton_log_u_guess_cgs = 12;
 
@@ -494,6 +494,8 @@ void cooling_cool_part(const struct phys_const *phys_const,
 #endif
 
   /* Get internal energy at the last kick step */
+  /* MATTHIEU: Is this really what we want? Shouldn't it be the drifted energy?
+   */
   const float u_start = hydro_get_physical_internal_energy(p, xp, cosmo);
 
   /* Get the change in internal energy due to hydro forces */
@@ -507,7 +509,6 @@ void cooling_cool_part(const struct phys_const *phys_const,
   u_0 = max(u_0, hydro_properties->minimal_internal_energy);
 
   /* Convert to CGS units */
-  const double u_start_cgs = u_start * cooling->internal_energy_to_cgs;
   const double u_0_cgs = u_0 * cooling->internal_energy_to_cgs;
   const double dt_cgs = dt * units_cgs_conversion_factor(us, UNIT_CONV_TIME);
 
@@ -532,7 +533,7 @@ void cooling_cool_part(const struct phys_const *phys_const,
    * metal-free Helium mass fraction as per the Wiersma+08 definition */
   const float HeFrac = XHe / (XH + XHe);
 
-  /* convert Hydrogen mass fraction into Hydrogen number density */
+  /* convert Hydrogen mass fraction into physical Hydrogen number density */
   const double n_H =
       hydro_get_physical_density(p, cosmo) * XH / phys_const->const_proton_mass;
   const double n_H_cgs = n_H * cooling->number_density_to_cgs;
@@ -616,42 +617,24 @@ void cooling_cool_part(const struct phys_const *phys_const,
     }
   }
 
-  /* Expected change in energy over the next kick step
-     (assuming no change in dt) */
-  const double delta_u_cgs = u_final_cgs - u_start_cgs;
-
   /* Convert back to internal units */
-  double delta_u = delta_u_cgs * cooling->internal_energy_from_cgs;
+  double u_final = u_final_cgs * cooling->internal_energy_from_cgs;
 
   /* We now need to check that we are not going to go below any of the limits */
+
+  /* Absolute minimum */
+  const double u_minimal = hydro_properties->minimal_internal_energy;
+  u_final = max(u_final, u_minimal);
 
   /* Limit imposed by the entropy floor */
   const double A_floor = entropy_floor(p, cosmo, floor_props);
   const double rho = hydro_get_physical_density(p, cosmo);
   const double u_floor = gas_internal_energy_from_entropy(rho, A_floor);
+  u_final = max(u_final, u_floor);
 
-  /* Absolute minimum */
-  const double u_minimal = hydro_properties->minimal_internal_energy;
-
-  /* Largest of both limits */
-  const double u_limit = max(u_minimal, u_floor);
-
-  /* First, check whether we may end up below the minimal energy after
-   * this step 1/2 kick + another 1/2 kick that could potentially be for
-   * a time-step twice as big. We hence check for 1.5 delta_u. */
-  if (u_start + 1.5 * delta_u < u_limit) {
-    delta_u = (u_limit - u_start) / 1.5;
-  }
-
-  /* Second, check whether the energy used in the prediction could get negative.
-   * We need to check for the 1/2 dt kick followed by a full time-step drift
-   * that could potentially be for a time-step twice as big. We hence check
-   * for 2.5 delta_u but this time against 0 energy not the minimum.
-   * To avoid numerical rounding bringing us below 0., we add a tiny tolerance.
-   */
-  if (u_start + 2.5 * delta_u < 0.) {
-    delta_u = -u_start / (2.5 + rounding_tolerance);
-  }
+  /* Expected change in energy over the next kick step
+     (assuming no change in dt) */
+  const double delta_u = u_final - u_start;
 
   /* Turn this into a rate of change (including cosmology term) */
   const float cooling_du_dt = delta_u / dt_therm;
